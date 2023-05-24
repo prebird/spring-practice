224de01ff71ef26cfd6ce0a2bb6147247e549150
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-05-25 00:25:40 +0900

    feat: 환경에 따른 datasource 빈 등록 설정파일 반영

--- src/main/java/com/example/springpractice/config/DataSourceConfig.java
+++ src/main/java/com/example/springpractice/config/DataSourceConfig.java
@@ -1,6 +1,7 @@
 package com.example.springpractice.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -11,19 +12,27 @@ import org.springframework.core.env.Environment;
 import javax.sql.DataSource;
 
 @Configuration
-@PropertySource("classpath:/application-prod.properties")
 public class DataSourceConfig {
 
-    @Autowired
-    Environment env;
-
-    @Bean
-    public DataSource dataSource() {
-        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName(env.getProperty("spring.datasource.driver-class-name"));
-        dataSourceBuilder.url(env.getProperty("spring.datasource.url"));
-        dataSourceBuilder.username(env.getProperty("spring.datasource.username"));
-        dataSourceBuilder.password(env.getProperty("spring.datasource.password"));
-        return dataSourceBuilder.build();
-    }
+//    @Value("${spring.datasource.driver-class-name}")
+//    private String driverName;
+//
+//    @Value("${spring.datasource.url}")
+//    private String url;
+//
+//    @Value("${spring.datasource.username}")
+//    private String userName;
+//
+//    @Value("${spring.datasource.password}")
+//    private String password;
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+//        dataSourceBuilder.driverClassName(driverName);
+//        dataSourceBuilder.url(url);
+//        dataSourceBuilder.username(userName);
+//        dataSourceBuilder.password(password);
+//        return dataSourceBuilder.build();
+//    }
 }
--- src/test/java/com/example/springpractice/config/DataSourceConfigTest.java
+++ src/test/java/com/example/springpractice/config/DataSourceConfigTest.java
@@ -4,14 +4,22 @@ import com.example.springpractice.SpringPracticeApplication;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
 
 import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@ActiveProfiles("test")
 class DataSourceConfigTest {
 
     @Test
