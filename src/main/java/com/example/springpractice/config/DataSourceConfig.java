937d7fb8f61fcfc57d532382dc499a9247b836e4
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-05-24 23:09:48 +0900

    feat: datasource 빈 등록 설정파일 반영

--- src/main/java/com/example/springpractice/config/DataSourceConfig.java
+++ src/main/java/com/example/springpractice/config/DataSourceConfig.java
@@ -1,22 +1,29 @@
 package com.example.springpractice.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:/application.properties")
 public class DataSourceConfig {
+
+    @Autowired
+    Environment env;
 
     @Bean
     public DataSource dataSource() {
         DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("org.h2.Driver");
-        dataSourceBuilder.url("jdbc:h2:mem:test");
-        dataSourceBuilder.username("sa");
-        dataSourceBuilder.password("");
+        dataSourceBuilder.driverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        dataSourceBuilder.url(env.getProperty("spring.datasource.url"));
+        dataSourceBuilder.username(env.getProperty("spring.datasource.username"));
+        dataSourceBuilder.password(env.getProperty("spring.datasource.password"));
         return dataSourceBuilder.build();
     }
 }
