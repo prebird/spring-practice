073bd01097f70c9937b3f653a5c61bdcbbcd464b
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-06-03 00:55:07 +0900

    feat: jpa 테스트 추가

--- src/test/java/com/example/springpractice/jpaShop/domain/DomainTest.java
+++ src/test/java/com/example/springpractice/jpaShop/domain/DomainTest.java
@@ -1,15 +1,39 @@
 package com.example.springpractice.jpaShop.domain;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
+@Rollback(value = false)
 class DomainTest {
+
+    @PersistenceContext
+    private EntityManager em;
 
     @Test
     void entityTest() {
-    }
+        ClassRoom javaClass = ClassRoom.of("자바반");
+        em.persist(javaClass);
 
+        Student jiyu = Student.of("김지유");
+        Student jinjoo = Student.of("김진주");
+        em.persist(jiyu);
+        em.persist(jinjoo);
+
+        List<Student> students = List.of(jiyu, jinjoo);
+
+        javaClass.setStudents(students);
+    }
 }
