5e4acd00d57ddcfb9bc4281de0f3dd59b5640fa9
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-06-06 12:46:22 +0900

    feat: 엔티티 기능 추가 - 연관관계 편의 메서드 추가 - 정적 팩토리 메서드 추가 - toString 추가

--- src/main/java/com/example/springpractice/jpaShop/domain/Student.java
+++ src/main/java/com/example/springpractice/jpaShop/domain/Student.java
@@ -1,36 +1,58 @@
 package com.example.springpractice.jpaShop.domain;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@Builder
+@Getter
+@Setter
+@ToString(of = {"id", "name", "age"}) // toString은 가급적 연관관계가 앖는 필드만 수행합니다.
 public class Student {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue
     private Long id;
-
     private String name;
-
     private Integer age;
-
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "class_id", insertable = false, updatable = false)
     private ClassRoom classRoom;
 
-    public static Student of(String name) {
-        return Student.builder()
-                .name(name)
-                .build();
+    private Student(String name) {
+        this(name, 0);
+    }
+    private Student(String name, Integer age) {
+        this(name, age, null);
+    }
+    private Student(String name, Integer age, ClassRoom classRoom) {
+        this.name = name;
+        this.age = age;
+        if (this.classRoom != null) {
+            changeClassRoom(classRoom);
+        }
     }
 
-    public void setClassRoom(ClassRoom classRoom) {
+    /**
+     * 반을 변경합니다.
+     * 연관관계 편의 메서드: 양방향 연관관계인 ClassRoom에 학생을 추가해 줍니다.
+     * @param classRoom
+     */
+    private void changeClassRoom(ClassRoom classRoom) {
         this.classRoom = classRoom;
+        classRoom.getStudents().add(this);
     }
+
+    public static Student of(String name) {
+        return new Student(name);
+    }
+
+    public static Student of(String name, Integer age) {
+        return new Student(name, age);
+    }
+
+    public static Student of(String name, Integer age, ClassRoom classRoom) {
+        return new Student(name, age, classRoom);
+    }
+
 }
