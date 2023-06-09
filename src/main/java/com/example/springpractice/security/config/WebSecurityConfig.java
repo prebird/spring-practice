8025cd9e3d266ba62e368eed3619e267f24d3101
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-06-10 01:10:57 +0900

    feat(security): BasicAuth 추가

--- src/main/java/com/example/springpractice/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/springpractice/security/config/WebSecurityConfig.java
@@ -6,8 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Slf4j
@@ -23,7 +21,9 @@ public class WebSecurityConfig {
                 .antMatchers("/").permitAll()
                 .antMatchers("/mamegers/**").hasRole("MANAGER")
                 .antMatchers("/partners/**").hasRole("partners")
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
 
         return http.build();
     }
