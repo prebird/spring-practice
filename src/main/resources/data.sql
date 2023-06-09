5d2ea66b3edec2d489c6e0540536d8c67b0ee8c9
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-06-09 21:19:52 +0900

    feat(security): 데이터 초기화, 설정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into member(id, username, password, role) values(1L, 'user', '12', 'USER');
+insert into member(id, username, password, role) values(2L, 'partner', '12', 'PARTNER');
+insert into member(id, username, password, role) values(3L, 'admin', '12', 'ADMIN');
