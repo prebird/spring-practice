16040fa6182287007dd5222059f1ea0c09c1fff9
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-06-09 22:43:24 +0900

    feat(security): security 기능을 위해 비활성화

--- src/main/java/com/example/springpractice/config/AppConfigForConfig.java
+++ src/main/java/com/example/springpractice/config/AppConfigForConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
-public class AppConfig {
+public class AppConfigForConfig {
 
     @Bean
     public MyServiceImpl myService() {
--- src/main/java/com/example/springpractice/security/config/MvcConfig.java
+++ src/main/java/com/example/springpractice/security/config/MvcConfig.java
@@ -9,9 +9,9 @@ public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
         //registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
-        registry.addViewController("/admin").setViewName("admin");
-        registry.addViewController("/login").setViewName("login");
+//        registry.addViewController("/").setViewName("home");
+//        registry.addViewController("/hello").setViewName("hello");
+//        registry.addViewController("/admin").setViewName("admin");
+//        registry.addViewController("/login").setViewName("login");
     }
 }
