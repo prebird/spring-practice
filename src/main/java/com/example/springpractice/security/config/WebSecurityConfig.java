9f1b8e8bac23ff5e46da5afb47ffddf7726cc6dc
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-06-09 21:23:43 +0900

    feat(security): WebSecurityConfig

--- src/main/java/com/example/springpractice/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/springpractice/security/config/WebSecurityConfig.java
@@ -1,49 +1,20 @@
 package com.example.springpractice.security.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                    .antMatchers("/", "home")
-                    .permitAll()
-                    .and()
-                .authorizeRequests()
-                    .antMatchers("/admin")
-                    .hasRole("ADMIN")
-                .anyRequest()
-                    .authenticated()
-                    .and()
-                .formLogin()
-                .loginPage("/login")
-                    .permitAll()
-                    .and()
-                .logout()
-                    .permitAll();
+        log.info("-------securityFilterChain-------");
         return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("sa")
-                        .password("")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
     }
 }
