c3ec745a73d27670572afcc8212303eda328bc87
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-06-12 21:42:26 +0900

    feat(querydsl): repository 추가

---
+++ src/main/java/com/example/springpractice/querydsl/entity/CompanyRepository.java
@@ -0,0 +1,6 @@
+package com.example.springpractice.querydsl.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+}
