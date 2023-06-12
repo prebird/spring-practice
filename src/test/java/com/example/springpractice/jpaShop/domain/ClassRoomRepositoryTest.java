c21e426a8201f61c410e9158101acd3440c87112
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-06-12 21:10:24 +0900

    feat(jpa): testEntityManger 추가

--- src/test/java/com/example/springpractice/jpaShop/domain/ClassRoomRepositoryTest.java
+++ src/test/java/com/example/springpractice/jpaShop/domain/ClassRoomRepositoryTest.java
@@ -1,28 +1,53 @@
 package com.example.springpractice.jpaShop.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import java.util.List;
 
-import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.*;
+import static org.assertj.core.api.Assertions.*;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = Replace.NONE)
 class ClassRoomRepositoryTest {
     @Autowired
     private ClassRoomRepository classRoomRepository;
 
+    @Autowired
+    private TestEntityManager entityManager;
+
     @Test
     void saveTest() {
-
         ClassRoom javaClass = ClassRoom.of("자바반");
         classRoomRepository.save(javaClass);
 
         List<ClassRoom> classes = classRoomRepository.findAll();
         System.out.println(classes);
+    }
+
+    @Test
+    @DisplayName("id로 반을 조회한다.")
+    void findById_returnClassRoom() {
+        ClassRoom javaClass = ClassRoom.of("자바반");
+        ClassRoom savedOne = classRoomRepository.save(javaClass);
+
+        ClassRoom findClass = classRoomRepository.findById(savedOne.getId())
+                .orElseThrow(() -> new RuntimeException("반 조회에 실패했습니다."));
+
+        assertThat(findClass.getName()).isEqualTo("자바반");
+    }
+
+    @Test
+    @DisplayName("id로 반을 조회한다.")
+    void findById_returnClassRoom2() {
+        ClassRoom javaClass = entityManager.persistAndFlush(ClassRoom.of("자바반"));
+
+        ClassRoom findClass = classRoomRepository.findById(javaClass.getId())
+                .orElseThrow(() -> new RuntimeException("반 조회에 실패했습니다."));
+
+        assertThat(findClass.getName()).isEqualTo("자바반");
     }
 
 }
