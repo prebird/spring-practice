1f7f4a7be483afae84476bfa75eb77fc97edf6e6
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-06-17 17:14:40 +0900

    feat(querydsl): projection, dto 조회

---
+++ src/main/java/com/example/springpractice/querydsl/dto/EmployeeDto.java
@@ -0,0 +1,13 @@
+package com.example.springpractice.querydsl.dto;
+
+import lombok.Getter;
+
+@Getter
+public class EmployeeDto {
+  private Long id;
+  private String name;
+  private Integer age;
+  private String companyName;
+
+
+}
--- src/test/java/com/example/springpractice/querydsl/entity/EmployeeTest.java
+++ src/test/java/com/example/springpractice/querydsl/entity/EmployeeTest.java
@@ -1,7 +1,10 @@
 package com.example.springpractice.querydsl.entity;
 
+import com.example.springpractice.querydsl.dto.EmployeeDto;
 import com.example.springpractice.security.member.QMember;
 import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
@@ -21,115 +24,158 @@ import static org.assertj.core.api.Assertions.*;
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class EmployeeTest {
-    @Autowired
-    TestEntityManager testEm;
-    @Autowired
-    EntityManager em;
 
-    JPAQueryFactory queryFactory;
+  @Autowired
+  TestEntityManager testEm;
+  @Autowired
+  EntityManager em;
+
+  JPAQueryFactory queryFactory;
+
+  // 테스트 데이터
+  Employee jiyu;
+  Employee jinju;
+  Employee hee;
+  Employee ddang;
+  Company whiteHorse;
+  Company heeSters;
+
+  @BeforeEach
+  void init() {
+    // querydsl factory
+    queryFactory = new JPAQueryFactory(em);
 
     // 테스트 데이터
-    Employee jiyu;
-    Employee jinju;
-    Employee hee;
-    Employee ddang;
-    Company whiteHorse;
-    Company heeSters;
+    whiteHorse = testEm.persistAndFlush(Company.of("백마티비"));
+    heeSters = testEm.persistAndFlush(Company.of("희스터즈"));
 
-    @BeforeEach
-    void init() {
-        // querydsl factory
-        queryFactory = new JPAQueryFactory(em);
+    jiyu = testEm.persistAndFlush(Employee.of("김지유", 10, whiteHorse));
+    jinju = testEm.persistAndFlush(Employee.of("김진주", 20, whiteHorse));
+    hee = testEm.persistAndFlush(Employee.of("최희령", 25, heeSters));
+    ddang = testEm.persistAndFlush(Employee.of("땡희", 30, heeSters));
+  }
 
-        // 테스트 데이터
-        whiteHorse = testEm.persistAndFlush(Company.of("백마티비"));
-        heeSters = testEm.persistAndFlush(Company.of("희스터즈"));
+  @Test
+  void entity_select_test() {
+    String jpql =
+        "select e from Employee e " +
+            "where e.name = :name";
+    List<Employee> employees = em.createQuery(jpql, Employee.class)
+        .setParameter("name", "땡희")
+        .getResultList();
 
-        jiyu = testEm.persistAndFlush(Employee.of("김지유", 10, whiteHorse));
-        jinju = testEm.persistAndFlush(Employee.of("김진주", 20, whiteHorse));
-        hee = testEm.persistAndFlush(Employee.of("최희령", 25, heeSters));
-        ddang = testEm.persistAndFlush(Employee.of("땡희", 30, heeSters));
-    }
+    assertThat(employees).contains(ddang);
+  }
 
-    @Test
-    void entity_select_test() {
-        String jpql =
-                "select e from Employee e " +
-                "where e.name = :name";
-        List<Employee> employees = em.createQuery(jpql, Employee.class)
-                .setParameter("name", "땡희")
-                .getResultList();
+  @Test
+  void entity_select_querydsl() {
+    Employee ddang = testEm.persistAndFlush(Employee.of("땡희"));
+    // when
+    //QEmployee e = new QEmployee("e"); // static import 방식으로 변경
 
-        assertThat(employees).contains(ddang);
-    }
+    Employee result = queryFactory
+        .select(employee)
+        .from(employee)
+        .where(employee.name.eq("땡희"))     // 파라메터 바인딩
+        .fetchOne();
 
-    @Test
-    void entity_select_querydsl() {
-        Employee ddang = testEm.persistAndFlush(Employee.of("땡희"));
-        // when
-        //QEmployee e = new QEmployee("e"); // static import 방식으로 변경
+    assertThat(result).usingRecursiveComparison().isEqualTo(ddang);
+  }
 
-        Employee result = queryFactory
-                .select(employee)
-                .from(employee)
-                .where(employee.name.eq("땡희"))     // 파라메터 바인딩
-                .fetchOne();
+  @Test
+  void 리스트_조회() {
+    List<Employee> result = queryFactory
+        .selectFrom(employee)
+        .fetch();
+    assertThat(result).hasSize(4);
+  }
 
-        assertThat(result).usingRecursiveComparison().isEqualTo(ddang);
-    }
+  @Test
+  void 조건_여러개_검색() {
+    Employee result = queryFactory
+        .selectFrom(employee)
+        .where(employee.name.like("%희%"),
+            employee.age.eq(30))
+        .fetchOne();
 
-    @Test
-    void 리스트_조회() {
-        List<Employee> result = queryFactory
-                .selectFrom(employee)
-                .fetch();
-        assertThat(result).hasSize(4);
-    }
+    assertThat(result).isEqualTo(ddang);
+  }
 
-    @Test
-    void 조건_여러개_검색() {
-        Employee result = queryFactory
-                .selectFrom(employee)
-                .where(employee.name.like("%희%"),
-                        employee.age.eq(30))
-                .fetchOne();
+  @Test
+  void 결과_반환_메서드() {
 
-        assertThat(result).isEqualTo(ddang);
-    }
+    // 리스트
+    List<Employee> resultList = queryFactory
+        .selectFrom(employee)
+        .where(employee.name.eq("땡희"))
+        .fetch();
 
-    @Test
-    void 결과_반환_메서드() {
-        // 리스트
-        List<Employee> resultList = queryFactory
-                .selectFrom(employee)
-                .where(employee.name.eq("땡희"))
-                .fetch();
+    // 단건
+    Employee resultOne = queryFactory
+        .selectFrom(employee)
+        .where(employee.name.eq("땡희"))
+        .fetchOne();
 
-        // 단건
-        List<Employee> resultOne = queryFactory
-                .selectFrom(employee)
-                .where(employee.name.eq("땡희"))
-                .fetch();
+    // fetchFirst() : limit(1).fetchOne()
+    Employee resultFirst = queryFactory
+        .selectFrom(employee)
+        .where(employee.name.eq("땡희"))
+        .fetchFirst();
 
-        // fetchFirst() : limit(1).fetchOne()
-        Employee resultFirst = queryFactory
-                .selectFrom(employee)
-                .where(employee.name.eq("땡희"))
-                .fetchFirst();
+    // fetchResults() : 페이징 포함
+    QueryResults<Employee> pageResult = queryFactory
+        .selectFrom(employee)
+        .where(employee.name.eq("땡희"))
+        .fetchResults(); // deprecated..
 
-        // fetchResults() : 페이징 포함
-        QueryResults<Employee> pageResult = queryFactory
-                .selectFrom(employee)
-                .where(employee.name.eq("땡희"))
-                .fetchResults(); // deprecated..
+    // fetchCount() : count 쿼리
+    long count = queryFactory
+        .selectFrom(employee)
+        .where(employee.name.eq("땡희"))
+        .fetchCount();
+    // deprecated
+  }
 
-        // fetchCount() : count 쿼리
-        long count = queryFactory
-                .selectFrom(employee)
-                .where(employee.name.eq("땡희"))
-                .fetchCount();
-                    // deprecated
-    }
+  @Test
+  void projection_one_test() {
+    Integer age = queryFactory.select(employee.age)
+        .from(employee)
+        .where(employee.name.eq("땡희"))
+        .fetchOne();
 
-    
+    assertThat(age).isEqualTo(30);
+  }
+
+  @Test
+  void projection_more_test() {
+    Tuple result = queryFactory.select(employee.name, employee.age)
+        .from(employee)
+        .where(employee.name.like("땡희"))
+        .fetchOne();
+
+    String empName = result.get(employee.name);
+    Integer empAge = result.get(employee.age);
+
+    assertThat(empName).isEqualTo("땡희");
+    assertThat(empAge).isEqualTo(30);
+  }
+
+  @Test
+  void projection_dto_test() {
+    EmployeeDto dto = queryFactory.select(Projections.fields(EmployeeDto.class,
+            employee.id,
+            employee.name,
+            employee.age,
+            employee.company.name.as("companyName"))  // 별칭 지정
+        )
+        .from(employee)
+        .where(employee.name.like("땡희"))
+        .fetchOne();
+
+    assertThat(dto.getName()).isEqualTo("땡희");
+    assertThat(dto.getAge()).isEqualTo(30);
+    assertThat(dto.getCompanyName()).isEqualTo("희스터즈");
+  }
+
+
 }
