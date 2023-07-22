a542698a3f5c73a21b2ef324e1f7f546a2c86467
gyureal <yonggyu.jeong10@gmail.com>
2023-07-22 17:02:45 +0900

    fix: redisConfig redisPractice profile 일 때만 동작하도록 수정

--- src/main/java/com/example/springpractice/redisPractice/config/RedisConfig.java
+++ src/main/java/com/example/springpractice/redisPractice/config/RedisConfig.java
@@ -3,10 +3,12 @@ package com.example.springpractice.redisPractice.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
+@Profile("redisPractice")
 @Configuration
 public class RedisConfig {
   @Value("${spring.redis.host}")
